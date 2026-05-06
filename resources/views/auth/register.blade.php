01fa17d66def312e71608801561f7c9a67d5a0e6
Rencustodio05 <rencustodio05@gmail.com>
2026-05-07 01:41:10 +0800

    update

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="{{ asset('images/workzap-logo.png') }}">
     <title>Workzap - Register</title>
     @vite(['resources/css/login_register.css', 'resources/js/app.js'])
 </head>
